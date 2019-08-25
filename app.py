d954cb8e9eb6f922e181982e2c27421bda030e6b
Targgus <jlhenson13@gmail.com>
2019-08-25 14:06:29 -0600

    Added improved functionality

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import requests
 import main
+import json
 
 app = Flask(__name__)
 
@@ -8,26 +10,59 @@ headers = {
     'TRN-Api-Key':'cc0ff145-ca3a-4f12-82a0-7759c9927e52'
 }
 
-data = main.data.username
+# TODO move to other file called Utilities
+# build class
+class baseInfo():
+    def __init__(self, username, timeplayed, playerLevel, gearScore):
+        self.username = username
+        self.timeplayed = timeplayed
+        self.playerLevel = playerLevel
+        self.gearScore = gearScore
 
-@app.route('/')
+# use request library to return data
+def getBaseInfo(username, headers):
+    if username is not None:
+        r = requests.get('https://public-api.tracker.gg/v2/division-2/standard/profile/uplay/'+username, headers=headers)
+        data = json.loads(r.text)
+        data = data['data']
+        return data
+
+def populateClass(className, dataVariable):
+    objectName = className(
+                username = dataVariable['platformInfo']['platformUserHandle'],
+                timeplayed = dataVariable['segments'][0]['stats']['timePlayed']['displayValue'],
+                playerLevel = dataVariable['segments'][0]['stats']['highestPlayerLevel']['value'],
+                gearScore = dataVariable['segments'][0]['stats']['latestGearScore']['value']
+            )
+    return objectName
+
+ 
+
+@app.route("/")
 def index():
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
+@app.route('/', methods=['POST', 'GET'])
+def index1():
+    if request.method == 'POST':
+        
+        username = request.form['username']     
 
+        # check to see if username is empty
+        if len(username) == 0:
+            # handle for blank username
+            print("user name is none")
+            data = None
+            message = "Please input a Username"
+        else:
+            # call function to retrieve API info and assign to baseInfoData variable
+            baseInfoData = getBaseInfo(username, headers)
+            # call function to fill class with baseInfoData, return data, and assign to variable called data
+            data = populateClass(baseInfo, baseInfoData)
+            message = None
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return render_template('index.html', data=data, message=message)
+   
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+
+
 # header data
 headers = {
     'TRN-Api-Key':'cc0ff145-ca3a-4f12-82a0-7759c9927e52'
@@ -22,14 +24,18 @@ baseInfoData = getBaseInfo(username, headers)
 
 # build class
 class baseInfo():
-    def __init__(self, username, timeplayed):
+    def __init__(self, username, timeplayed, playerLevel, gearScore):
         self.username = username
         self.timeplayed = timeplayed
+        self.playerLevel = playerLevel
+        self.gearScore = gearScore
         
 # initialize class and populate data
 data = baseInfo(
     username = baseInfoData['platformInfo']['platformUserHandle'],
-    timeplayed = baseInfoData['segments'][0]['stats']['timePlayed']['displayValue']
+    timeplayed = baseInfoData['segments'][0]['stats']['timePlayed']['displayValue'],
+    playerLevel = baseInfoData['segments'][0]['stats']['highestPlayerLevel']['value'],
+    gearScore = baseInfoData['segments'][0]['stats']['latestGearScore']['value']
     )
 
 
